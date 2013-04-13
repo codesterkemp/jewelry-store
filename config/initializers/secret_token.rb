88bcb11a7868cc700382c5573986877a88bca3e9
codesterkemp <sparky_csk@yahoo.com>
2013-04-13 15:58:01 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jeak::Application.config.secret_token = '3cd3d0635b40c93124061adb4686a4bfd84be254056dfa7f61d2d42ebd6dff0b079bc0ef2ef6dbc11019430a13718f1987d44a97e3c5bc9a66455711d4e53612'
